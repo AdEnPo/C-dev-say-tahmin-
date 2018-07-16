ca4d8cb8f813617bb0d5439f3c43a615d7a5d802
adem enes polat <ademenespolat@gmail.com>
2018-07-16 12:07:22 +0300

    odev

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Tahmin Et";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(22, 92);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(154, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(34, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(102, 133);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(34, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(142, 133);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(34, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(22, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 10);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(62, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 10);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(102, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 10);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Location = new System.Drawing.Point(142, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 10);
+            this.label6.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 189);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(154, 186);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 383);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] soru;
+        int tahmin;
+        int[] ttahmin = new int[4];
+        int i, j, index = 1;
+        int yerinde = 0, ydat = 0;
+        int tahmintut;
+        int arti = 0, eksi = 0;
+        private void sayitut()
+        {
+            i=0;
+            soru = new int[4];
+            Random rnd = new Random();
+
+            //tahmin edilicek sayı 
+            while ( i < 4)
+            {
+                soru[i] = rnd.Next(0, 10);
+
+                if (i == 0 && soru[0] == 0)
+                    continue;
+                if (i == 1 && soru[1] == soru[0])
+                    continue;
+                if(i == 2 && (soru[2] == soru[1] || soru[2] == soru[0]))
+                    continue;
+                if (i == 3 && (soru[3] == soru[2] || soru[3] == soru[1] || soru[3] == soru[0])) 
+                    continue;
+                i++;
+            }
+     
+        }
+    
+        private void oyun()
+        {
+            do
+            {
+                tahmin = (int)numericUpDown1.Value;
+                if (10000 < tahmin || tahmin < 1000)
+                {
+                    MessageBox.Show("Bu Deger Limitlerin Disinda ..");
+                    return;
+                }
+            }
+            while (tahmin > 10000 || tahmin < 1000);
+
+            tahmintut = tahmin;
+            //tahmin sayısını diziye atadık
+            ttahmin[0] = tahmin / 1000;
+            tahmin = tahmin % 1000;
+            // MessageBox.Show(" "+ ttahmin[0]+"    "+ tahmin);
+            ttahmin[1] = tahmin / 100;
+            tahmin = tahmin % 100;
+            // MessageBox.Show(" "+ ttahmin[1]+"    "+ tahmin);
+            ttahmin[2] = tahmin / 10;
+            tahmin = tahmin % 10;
+            //  MessageBox.Show(" " + ttahmin[2] + "    " + tahmin);
+            ttahmin[3] = tahmin;
+            // MessageBox.Show(" " + ttahmin[3] + "    " + tahmin);
+
+            textBox1.Text = Convert.ToString(ttahmin[0]);
+            textBox2.Text = Convert.ToString(ttahmin[1]);
+            textBox3.Text = Convert.ToString(ttahmin[2]);
+            textBox4.Text = Convert.ToString(ttahmin[3]);
+
+
+
+            if (ttahmin[1] == ttahmin[0])
+            {
+                MessageBox.Show("Girdiginiz sayida hata var. yeniden deneyin..");
+                return;
+            }
+            if (ttahmin[2] == ttahmin[1] || ttahmin[2] == ttahmin[0])
+            {
+                MessageBox.Show("Girdiginiz sayida hata var. yeniden deneyin..");
+                return;
+            }
+            if (ttahmin[3] == ttahmin[2] || ttahmin[3] == ttahmin[1] || ttahmin[3] == ttahmin[0])
+            {
+                MessageBox.Show("Girdiginiz sayida hata var. yeniden deneyin..");
+                return;
+            }
+
+            if (ttahmin[0] == soru[0])
+                label3.BackColor = Color.Green;
+            if (ttahmin[1] == soru[1])
+                label4.BackColor = Color.Green;
+            if (ttahmin[2] == soru[2])
+                label5.BackColor = Color.Green;
+            if (ttahmin[3] == soru[3])
+                label6.BackColor = Color.Green;
+
+            if (ttahmin[0] == soru[1] || ttahmin[0] == soru[2] || ttahmin[0] == soru[3])
+                label3.BackColor = Color.Blue;
+            if (ttahmin[1] == soru[0] || ttahmin[1] == soru[2] || ttahmin[1] == soru[3])
+                label4.BackColor = Color.Blue;
+            if (ttahmin[2] == soru[1] || ttahmin[2] == soru[0] || ttahmin[2] == soru[3])
+                label5.BackColor = Color.Blue;
+            if (ttahmin[3] == soru[1] || ttahmin[3] == soru[2] || ttahmin[3] == soru[0])
+                label6.BackColor = Color.Blue;
+
+            if (ttahmin[0] != soru[1] && ttahmin[0] != soru[2] && ttahmin[0] != soru[3] && ttahmin[0] != soru[0])
+                label3.BackColor = Color.Yellow;
+            if (ttahmin[1] != soru[0] && ttahmin[1] != soru[2] && ttahmin[1] != soru[3] && ttahmin[1] != soru[1])
+                label4.BackColor = Color.Yellow;
+            if (ttahmin[2] != soru[1] && ttahmin[2] != soru[0] && ttahmin[2] != soru[3] && ttahmin[2] != soru[2])
+                label5.BackColor = Color.Yellow;
+            if (ttahmin[3] != soru[1] && ttahmin[3] != soru[2] && ttahmin[3] != soru[0] && ttahmin[3] != soru[3])
+                label6.BackColor = Color.Yellow;
+
+
+            for (j = 0; j < 4; j++)
+            {
+
+                if (ttahmin[j] == soru[0] || ttahmin[j] == soru[1] || ttahmin[j] == soru[2] || ttahmin[j] == soru[3])
+                {
+
+                    if (soru[j] != ttahmin[j])
+                    {
+                        ydat += 1;
+                        //	printf("\n%d",i); which one
+                    }
+                    if (ttahmin[j] == soru[j])
+                    {
+                        yerinde += 1;
+                        //	printf("\t%d\n",i); which one
+                    }
+                    eksi += ydat;
+                    arti += yerinde;
+                }
+            }
+            listBox1.Items.Add(index++ + ") " + tahmintut + "\t       " + " - " + ydat + "  " + " + " + yerinde);
+            if (yerinde == 4)
+            {
+                MessageBox.Show("tebrikler kazandın");
+                index = 0;
+                listBox1.Items.Clear();
+                numericUpDown1.Value = 0;
+                textBox1.Clear();   textBox2.Clear();   textBox3.Clear();   textBox4.Clear();
+                label3.BackColor = Color.Yellow;    label4.BackColor = Color.Yellow;
+                label5.BackColor = Color.Yellow;    label6.BackColor = Color.Yellow;
+                button2.Enabled = false;    numericUpDown1.Enabled = false;
+            }
+            ydat = 0;
+            yerinde = 0;
+            //hakları saydır
+            if (index > 11)
+            {
+                index = 0;
+                listBox1.Items.Clear();
+                numericUpDown1.Value = 0;
+                MessageBox.Show("hakların bitti...");
+                textBox1.Clear();   textBox2.Clear();   textBox3.Clear();   textBox4.Clear();
+                label3.BackColor = Color.Yellow;    label4.BackColor = Color.Yellow;
+                label5.BackColor = Color.Yellow;    label6.BackColor = Color.Yellow;
+                button2.Enabled = false;    numericUpDown1.Enabled = false;
+            }
+        }
+private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            numericUpDown1.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            oyun();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayitut();
+            button2.Enabled = true;
+            numericUpDown1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            oyun();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
